8d339ede5608d0e23a0229067fe983088a231580
Joao Paulo <oliveirajpufrj@gmail.com>
2022-12-14 00:05:09 -0300

    fix: add prismaservice to providers

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma.service';
 import { UserController } from './user.controller';
 import { UsuarioRepository } from './user.repository';
 
 @Module({
   controllers: [UserController],
-  providers: [UsuarioRepository],
+  providers: [UsuarioRepository, PrismaService],
 })
 export class UserModule {}
